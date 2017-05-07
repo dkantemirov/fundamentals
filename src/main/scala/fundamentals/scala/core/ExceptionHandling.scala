0bb33debdc9a05369097c8a281b22f7d3f5004b5
Kantemirov David <it@kantemirov.ru>
2017-05-07 15:19:05 +0300

    Added Loan pattern

---
+++ src/main/scala/fundamentals/scala/core/ExceptionHandling.scala
@@ -0,0 +1,14 @@
+package fundamentals.scala.core
+
+import java.io.IOException
+
+object ExceptionHandling extends App {
+  try throw new Exception catch {case e: Throwable => println(e)} finally println("fin")
+}
+
+object MultiCatchBlock extends App {
+  try throw new Exception catch {
+    case e: IOException => println(e)
+    case e: Throwable => println(e)
+  }
+}
