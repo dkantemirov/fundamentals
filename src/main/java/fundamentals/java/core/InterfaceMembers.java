8719b06dc939c646f67a324c0e81a8b36af081ab
Kantemirov David <it@kantemirov.ru>
2017-05-08 15:19:02 +0300

    Scala vs Java Enumeration

--- src/main/java/fundamentals/java/core/InterfaceMembers.java
+++ src/main/java/fundamentals/java/core/InterfaceMembers.java
@@ -27,12 +27,12 @@ interface MethodsDeclarations {
     // An abstract method
     int m1();
 
-    // A static method
+    // A static method (Java 8)
     static int m2() {
         return 1;
     }
 
-    // A default method
+    // A default method (Java 8)
     // All classes implementing the interface will inherit the default implementation
     default void m3() {
     }
@@ -60,11 +60,33 @@ interface ATM {
     boolean withdraw(double amount) throws InsufficientFundException;
     double getBalance();
 
-    // Card is a nested interface. You can omit the keywors public and static.
+    // Card is a nested interface
+    // All nested types are implicitly public and static
     public static interface Card {
         String getNumber();
         String getSecurityCode();
         LocalDate getExpirationDate();
         String getCardHolderName();
     }
+}
+
+/**
+ * The Job Interface with a Nested Class and a Constant Field
+ */
+interface Job {
+    // A nested class
+    class EmptyJob implements Job {
+        private EmptyJob() {
+            // Do not allow outside to create its object
+        }
+
+        public void runJob() {
+            System.out.println("Nothing serious to run...");
+        }
+    }
+
+    // A constant field
+    Job EMPTY_JOB = new EmptyJob();
+    // An abstract method
+    void runJob();
 }
