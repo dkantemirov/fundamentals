14cd99d4f50fee34420a2e0cd2790816f0d0ff58
Kantemirov David <it@kantemirov.ru>
2017-05-11 12:24:06 +0300

    Java Function package overview

---
+++ src/main/java/fundamentals/java/lambda/IntersectionType.java
@@ -0,0 +1,16 @@
+package fundamentals.java.lambda;
+
+import java.io.Serializable;
+
+interface Sensitive {
+    // It ia a marker interface. So, no methods exist.
+}
+
+public class IntersectionType {
+    public static void main(String[] args) {
+        // Example with custom marker interface
+        Sensitive sen = (Sensitive & Adder) (x, y) -> x + y;
+
+        Serializable ser = (Serializable & Adder) (x, y) -> x + y;
+    }
+}
